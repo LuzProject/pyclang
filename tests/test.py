4f668f56d683d31a195ed83cb969850e2a82a0f3
jaidan <ajaidan0@gmail.com>
2023-01-27 08:49:05 -0500

    feat (TESTS): add swift compile to test

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,8 @@
-from pyclang import CCompiler
+from pathlib import Path
+from pyclang import CCompiler, SwiftCompiler
 
 c = CCompiler().set_compiler('/usr/bin/cc').add_arg('-O0')
-c.compile('test.c')
+c.compile(Path('test.c'))
+
+swift = SwiftCompiler()
+swift.compile(Path('test.swift'))

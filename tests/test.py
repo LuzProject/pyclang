a72548ccae3147c71625ee1e46dcdf41265316d0
jaidan <ajaidan0@gmail.com>
2023-01-27 08:50:20 -0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from pyclang import CCompiler, SwiftCompiler
 
 c = CCompiler().set_compiler('/usr/bin/cc').add_arg('-O0')
-c.compile(Path('test.c'))
+c.compile(Path('test.c'), 'test-c.o')
 
 swift = SwiftCompiler()
-swift.compile(Path('test.swift'))
+swift.compile(Path('test.swift'), 'test-swift.o')
